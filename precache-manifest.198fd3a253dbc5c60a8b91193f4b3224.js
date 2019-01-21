ecd50af54ffcc443d1bfc6b1f80848ccf1c6d69d
Frej <frej@Frejs-MBP-3.lan>
2019-01-21 06:38:37 +0100

    Updates

---
+++ precache-manifest.198fd3a253dbc5c60a8b91193f4b3224.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee11b86ae35c26898408",
+    "url": "/hackernews-tdd/static/js/runtime~main.ee11b86a.js"
+  },
+  {
+    "revision": "946f0e9102b8d0b47cb3",
+    "url": "/hackernews-tdd/static/js/main.946f0e91.chunk.js"
+  },
+  {
+    "revision": "453b1c13776d2fc07dac",
+    "url": "/hackernews-tdd/static/js/1.453b1c13.chunk.js"
+  },
+  {
+    "revision": "946f0e9102b8d0b47cb3",
+    "url": "/hackernews-tdd/static/css/main.a44b23cb.chunk.css"
+  },
+  {
+    "revision": "b23f0787e16c95a1733a2d7b0e0ac715",
+    "url": "/hackernews-tdd/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/hackernews-tdd/precache-manifest.198fd3a253dbc5c60a8b91193f4b3224.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/hackernews-tdd/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
